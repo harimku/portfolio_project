24573bb75ddc6fb04ed7600287da11497b67b57c
Ha Rim Ku <hrku19@gmail.com>
2020-08-10 12:50:51 -0700

    Set up Express Router

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express'); 
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const homedecorRouter = require('./routes/homedecorRouter');
+const automotiveRouter = require('./routes/automotiveRouter');
+/*
+const electronicsRouter = require('./routes/electronicsRouter');
+const fashionRouter = require('./routes/fashionRouter');
+const outdoorsRouter = require('./routes/outdoorsRouter');
+const petRouter = require('./routes/petRouter');
+const cartRouter = require('./routes/cartRouter');
+*/
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/homedecor', homedecorRouter);
+app.use('/automotive', automotiveRouter);
+/*
+app.use('/electronics', electronicsRouter);
+app.use('/fashion', fashionRouter);
+app.use('/outdoors', outdoorsRouter);
+app.use('/pet', petRouter);
+app.use('/cart', cartRouter);
+*/
 
 app.use(express.static(__dirname + '/public'));  //get express to serve files from public folder
 
